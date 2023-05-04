8bcdd06956946109ee8963585c7101eeddb108c4
aprxty3 <ajiprxty3@gmail.com>
2023-05-04 20:40:47 +0700

    create splash screen

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:fugi_diary_apps/screen/main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 4),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MainScreen(),
+        ),
+      ),
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          color: const Color(0xff96DAF0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeInDown(
+                child: Icon(
+                  Icons.language,
+                  color: Colors.white,
+                  size: MediaQuery.of(context).size.width * 0.3,
+                ),
+              ),
+              const SizedBox(height: 10),
+              FadeInUp(
+                duration: const Duration(seconds: 2),
+                child: Text(
+                  'Diary Apps',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
